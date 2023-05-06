816234685e85dfb340dcc6acdd3440379c28b5cd
disisisid <sidnshady@gmail.com>
2023-05-07 03:24:44 +0530

    Created User Listing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  let usersUrl = "https://jsonplaceholder.typicode.com/users";
+
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
+    fetch(usersUrl)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(d => {
+        d.address = d.address.city;
+        d.company = d.company.name;        
+      })
+      setUsers(data)
+    });
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{/*      
+      <ul>
+        {users.map((user) => (<li>{user.name}</li>))}
+      </ul>
+*/}
+      <table>
+        <tr>
+          {users[0]?.id && Object.keys(users[0]).map((k, i) =>
+            (
+            <th key={i}>{k.slice(0,1).toUpperCase() + k.slice(1,k.length)}</th>
+            ))
+          }
+        </tr>
+        {users.map((u, i) =>
+        (<tr key={i}>
+          {Object.keys(u).map(k => <td key={k} onClick={() => console.log(u)} onMouseOver={() => console.log(u)} >{u[k]}</td>)}
+        </tr>))
+        }
+      </table>
     </div>
   );
 }
